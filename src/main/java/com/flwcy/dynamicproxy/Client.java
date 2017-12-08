2a1f315ce71d5417f9873c12fcb24fb77ea8851b
junran.jiang <junran.jiang@payegis.com>
2017-12-08 17:04:10 +0800

    update about dynamic proxy

--- src/main/java/com/flwcy/dynamicproxy/Client.java
+++ src/main/java/com/flwcy/dynamicproxy/Client.java
@@ -16,11 +16,11 @@ public class Client {
         // 我们要代理的真实对象
         RealSubject realSubject = new RealSubject();
         /**
-         * DynamicProxy 实现了 InvocationHandler 接口，并能实现方法调用从代理类到委托类的分派转发
+         * InvocationHandlerImpl 实现了 InvocationHandler 接口，并能实现方法调用从代理类到委托类的分派转发
          * 其内部通常包含指向委托类实例的引用，用于真正执行分派转发过来的方法调用.
          * 即：要代理哪个真实对象，就将该对象传进去，最后是通过该真实对象来调用其方法
          */
-        InvocationHandler handler = new DynamicProxy(realSubject);
+        InvocationHandler handler = new InvocationHandlerImpl(realSubject);
         // 通过Proxy的newProxyInstance方法来创建我们的代理对象
         Subject subject = (Subject) Proxy.newProxyInstance(handler.getClass().getClassLoader(),realSubject.getClass().getInterfaces(),handler);
 
@@ -42,9 +42,9 @@ public class Client {
         BufferedOutputStream out = null;
         File file = new File(String.format("E:/tmp/%s.class",proxyName));
 
-                    /*
-             * Generate the specified proxy class.
-             */
+        /*
+         * Look up or generate the designated proxy class.
+         */
         byte[] proxyClassFile = ProxyGenerator.generateProxyClass(
                 proxyName, interfaces, Modifier.PUBLIC);
 
